16d50b07e8a5c2f1e3f809e2bf99260cc68f416f
ipeng6 <ipeng6@qq.com>
2022-07-17 03:05:28 +0800

    ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const withMDX = require('@next/mdx')({
 });
 
 const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
