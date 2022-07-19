969da318a021fedbe5dd40b9ce5664b25fa7638d
ipeng6 <ipeng6@qq.com>
2022-07-20 01:22:33 +0800

    fix path

--- pages/api/sidebar.ts
+++ pages/api/sidebar.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { promises as fs } from 'node:fs';
+import path from 'node:path';
 
 type Data = any;
 
@@ -8,7 +9,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const text = await fs.readFile('pages/_sidebar.mdx');
+  const text = await fs.readFile(path.resolve('pages/_sidebar.mdx'));
   res.setHeader('cache-control', 'max-age=300');
   res.end(text);
 }
